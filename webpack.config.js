f52b34ac4b75a28ac960ebed296e90a2861622ae
natirivero <riveronati@gmail.com>
2022-06-09 10:21:44 -0500

    problemas para generar el index.html en la carpeta /dist

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,14 @@
-const path = require("path");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.export = {
-  entry: "./src/index.js",
+  entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "main.js",
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
   },
   resolve: {
-    extensions: [".js"],
+    extensions: ['.js', '.mjs'],
   },
   module: {
     rules: [
@@ -15,9 +16,17 @@ module.export = {
         test: /\.m?js$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
         },
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: true,
+      inject: 'body', //posicion de la insercion
+      template: './public/index.html',
+      filename: './index.html',
+    }),
+  ],
 };
